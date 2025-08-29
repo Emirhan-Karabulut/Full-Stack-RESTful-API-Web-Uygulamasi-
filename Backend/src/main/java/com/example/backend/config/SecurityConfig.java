6bf3b419cff3e8b8038990fdaea4d66cf178c5ca
Emirhan Karabulut <karabulutemirhan091@gmail.com>
2025-08-29 17:22:08 +0300

    Initial commit

--- Backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -91,10 +91,7 @@ public class SecurityConfig {
 
         try {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of(
-                    "https://emirhan-karabulut.github.io",
-                    "http://localhost:4200"
-            ));
+            configuration.setAllowedOrigins(List.of("http://localhost:4200"));
             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(List.of("*"));
             configuration.setAllowCredentials(true);
